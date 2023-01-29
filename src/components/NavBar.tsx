f3aecf2d0453c45b0533ae8b0cf3a67ee35ff1ca
HatulaPro <shalev280804@gmail.com>
2023-01-29 18:45:55 +0200

    Fixing auth

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,33 @@
 import { A } from '@solidjs/router';
 import { Component } from 'solid-js';
 import { useLocation } from 'solid-start';
+import { useSession } from '~/db/useSession';
+import { signIn, signOut } from '@auth/solid-start/client';
 
 export const NavBar: Component = () => {
-	const location = useLocation();
-	const active = (path: string) => (path == location.pathname ? 'border-sky-600' : 'border-transparent hover:border-sky-600');
+	const user = useSession();
 
 	return (
-		<nav class="fixed top-0 z-50 w-full bg-gray-700 text-white">
-			<ul class="container flex items-center p-4">
-				<li class={`border-b-2 ${active('/')} mx-1.5 sm:mx-6`}>
-					<A href="/">Home</A>
-				</li>
-			</ul>
+		<nav class="fixed top-0 z-50 w-full border-b-2 border-gray-700 bg-gray-900 text-white">
+			<div class="container flex items-center justify-end p-4">
+				<A class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} href="/">
+					Home
+				</A>
+				{user.latest ? (
+					<>
+						<A class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} href="/profile">
+							Profile
+						</A>
+						<button class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} onClick={() => signOut()}>
+							Sign Out
+						</button>
+					</>
+				) : (
+					<button class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} onClick={() => signIn()}>
+						Sign In
+					</button>
+				)}
+			</div>
 		</nav>
 	);
 };
