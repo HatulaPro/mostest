25481d042aec5958b2b1c73e0ef792932282f764
HatulaPro <shalev280804@gmail.com>
2023-02-04 16:02:29 +0200

    Adding alt to image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { A } from '@solidjs/router';
+import { A } from 'solid-start';
 import type { Component } from 'solid-js';
 import { useSession } from '~/db/useSession';
 import { signIn, signOut } from '@auth/solid-start/client';
@@ -9,23 +9,23 @@ export const NavBar: Component = () => {
 	return (
 		<nav class="fixed top-0 z-50 flex w-full items-center border-b-2 border-gray-700 bg-gray-900 text-white">
 			<A href="/">
-				<img src="/logo.svg" class="m-2 max-h-[2rem] object-contain sm:max-h-[2.8rem]" />
+				<img src="/logo.svg" class="m-2 max-h-[2rem] object-contain sm:max-h-[2.8rem]" alt="Mostest logo" />
 			</A>
 			<div class="container flex items-center justify-end p-4">
-				<A class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} href="/">
+				<A class="mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6" href="/">
 					Home
 				</A>
 				{user.latest ? (
 					<>
-						<A class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} href="/profile">
+						<A class="mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6" href="/profile">
 							Profile
 						</A>
-						<button class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} onClick={() => signOut()}>
+						<button class="mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6" onClick={() => signOut()}>
 							Sign Out
 						</button>
 					</>
 				) : (
-					<button class={`mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6`} onClick={() => signIn()}>
+					<button class="mx-1.5 transition-colors duration-500 hover:text-red-400 sm:mx-6" onClick={() => signIn()}>
 						Sign In
 					</button>
 				)}
