3bdd0c94c5060b13199dad76bec96ac7976e4304
HatulaPro <shalev280804@gmail.com>
2023-02-04 16:08:51 +0200

    Fixing profile view on mobile

--- src/routes/profile/(profile).tsx
+++ src/routes/profile/(profile).tsx
@@ -28,24 +28,22 @@ export default function ProfilePage() {
 		<main class="text-center">
 			<Suspense
 				fallback={
-					<>
-						<div class="mx-auto flex max-w-5xl flex-col p-2 pt-20">
-							<h1 class="mt-12 text-2xl text-white sm:text-3xl md:text-4xl">Your Leaderboards</h1>
-							<div class="mx-auto mt-12 flex w-full flex-col overflow-hidden rounded-md border-2 border-gray-500 text-left">
-								<For each={[1, 2, 3]}>
-									{() => (
-										<div class="flex items-center gap-2 p-3 hover:bg-black hover:bg-opacity-20">
-											<span class="animate-pulse bg-slate-700 text-lg font-bold text-transparent">NAME OF THING</span>
-											<span class="animate-pulse bg-slate-700 text-base text-transparent">SOME QUESTION ABOUT IT?</span>
-											<div class="ml-auto flex h-8 gap-2">
-												<For each={[1, 2, 3, 4]}>{() => <div class="h-full w-8 animate-pulse bg-slate-700 object-contain text-transparent"></div>}</For>
-											</div>
+					<div class="mx-auto flex max-w-5xl flex-col p-2 pt-20">
+						<h1 class="mt-12 text-2xl text-white sm:text-3xl md:text-4xl">Your Leaderboards</h1>
+						<div class="mx-auto mt-12 flex w-full flex-col overflow-hidden rounded-md border-2 border-gray-500 text-left">
+							<For each={[1, 2, 3]}>
+								{() => (
+									<div class="flex items-center gap-2 p-3 hover:bg-black hover:bg-opacity-20">
+										<span class="animate-pulse bg-slate-700 text-lg font-bold text-transparent">NAME OF THING</span>
+										<span class="animate-pulse bg-slate-700 text-base text-transparent">SOME QUESTION ABOUT IT?</span>
+										<div class="ml-auto flex h-8 gap-2">
+											<For each={[1, 2, 3, 4]}>{() => <div class="h-full w-8 animate-pulse bg-slate-700 object-contain text-transparent"></div>}</For>
 										</div>
-									)}
-								</For>
-							</div>
+									</div>
+								)}
+							</For>
 						</div>
-					</>
+					</div>
 				}
 			>
 				{data.latest ? (
@@ -60,8 +58,8 @@ export default function ProfilePage() {
 							<div class="mx-auto mt-12 flex w-full flex-col overflow-hidden rounded-md border-2 border-gray-500 text-left">
 								<For each={data.latest.leaderboards}>
 									{(leaderboard) => (
-										<div class="flex items-center gap-2 p-3 hover:bg-black hover:bg-opacity-20">
-											<A href={`/${leaderboard.slug}`} class="text-lg font-bold hover:underline">
+										<div class="flex flex-col items-start gap-2 border-b-2 border-gray-700 p-3 last:border-b-0 hover:bg-black hover:bg-opacity-20 sm:flex-row sm:items-center">
+											<A href={`/${leaderboard.slug}`} class="text-lg font-bold hover:underline ">
 												{leaderboard.name} <span class="text-base font-normal text-gray-300">{leaderboard.question}</span>
 											</A>
 											<div class="ml-auto flex h-8 gap-2">
