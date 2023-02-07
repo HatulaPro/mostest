338f80d0b9f5ebe996daa1edaeb62dc3853f277e
HatulaPro <shalev280804@gmail.com>
2023-02-07 12:21:51 +0200

    Using stores for forms

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,40 +1,46 @@
-import { createSignal, createMemo } from 'solid-js';
+import { createStore } from 'solid-js/store';
+import { createMemo } from 'solid-js';
 import type { z } from 'zod';
 
-function createFormState<TField extends string>(fields: Record<TField, Field>) {
-	const entries = Object.entries(fields) as [TField, Field][];
-	const mapped = entries.map(([fieldName, field]) => [fieldName, { parser: field.parser, touched: false, value: field.defaultValue ?? '', error: null }]);
-	const result = Object.fromEntries(mapped);
-	return result as Record<TField, { parser: z.ZodString; touched: boolean; value: string; error: string | null }>;
-}
-
+type Data = {
+	parser: z.ZodString;
+	touched: boolean;
+	value: string;
+	error: string | null;
+};
 type Field = {
 	parser: z.ZodString;
 	defaultValue?: string;
 };
+function createFormState<TField extends string>(fields: Record<TField, Field>) {
+	const entries = Object.entries(fields) as [TField, Field][];
+	const mapped = entries.map(([fieldName, field]) => [fieldName, { parser: field.parser, touched: false, value: field.defaultValue ?? '', error: null }] as const);
+	const result = Object.fromEntries(mapped);
+	return result as Record<TField, Data>;
+}
 
 export function useForm<TField extends string>(fields: Record<TField, Field>) {
-	const [formState, setFormState] = createSignal(createFormState(fields));
+	const [formState, setFormState] = createStore(createFormState(fields));
 
 	const getError = (field: TField) => {
-		return formState()[field].error;
+		return formState[field].error;
 	};
 
 	const getValue = (field: TField) => {
-		return formState()[field].value;
+		return formState[field].value;
 	};
 
 	const setValue = (field: TField, newValue: string) => {
-		setFormState((p) => {
-			const parsed = p[field].parser.safeParse(newValue);
-			p[field] = { ...p[field], value: newValue, error: parsed.success ? null : parsed.error.flatten().formErrors.join('. '), touched: true };
-			return { ...p };
+		// eslint-disable-next-line
+		setFormState(field as any, (p) => {
+			const parsed = p.parser.safeParse(newValue);
+			return { ...p, value: newValue, error: parsed.success ? null : parsed.error.flatten().formErrors.join('. '), touched: true };
 		});
 	};
 	const isValid = createMemo(() => {
-		for (const k in formState()) {
+		for (const k in formState) {
 			const key = k as TField;
-			const parsed = formState()[key].parser.safeParse(getValue(key));
+			const parsed = formState[key].parser.safeParse(getValue(key));
 			if (!parsed.success) {
 				return false;
 			}
@@ -43,7 +49,7 @@ export function useForm<TField extends string>(fields: Record<TField, Field>) {
 	});
 
 	const data = () => {
-		const entries = Object.entries(formState()) as [TField, { parser: z.ZodString; touched: boolean; value: string; error: string | null }][];
+		const entries = Object.entries(formState) as [TField, { parser: z.ZodString; touched: boolean; value: string; error: string | null }][];
 		const mapped = entries.map(([fieldName, value]) => [fieldName, value.value]);
 		const result = Object.fromEntries(mapped);
 		return result as Record<TField, string>;
