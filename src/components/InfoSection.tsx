afff93c01e1fd6505cb59607263aecd2881736c8
HatulaPro <shalev280804@gmail.com>
2023-02-08 16:30:20 +0200

    Reducing bundle size

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,8 +1,10 @@
-import type { IconTypes } from 'solid-icons';
-import { AiOutlineArrowRight, AiOutlineControl, AiOutlineShareAlt, AiOutlineUnorderedList } from 'solid-icons/ai';
-import type { JSX, Component } from 'solid-js';
-import { Dynamic } from 'solid-js/web';
+import { type JSX, type Component, lazy } from 'solid-js';
 import { A } from 'solid-start';
+
+const AiOutlineArrowRight = lazy(async () => ({ default: (await import('solid-icons/ai')).AiOutlineArrowRight }));
+const AiOutlineControl = lazy(async () => ({ default: (await import('solid-icons/ai')).AiOutlineControl }));
+const AiOutlineShareAlt = lazy(async () => ({ default: (await import('solid-icons/ai')).AiOutlineShareAlt }));
+const AiOutlineUnorderedList = lazy(async () => ({ default: (await import('solid-icons/ai')).AiOutlineUnorderedList }));
 
 export const InfoSection: Component = () => {
 	return (
@@ -12,7 +14,7 @@ export const InfoSection: Component = () => {
 				<h2 class="my-4 text-3xl font-bold sm:text-4xl md:text-5xl">Community Leaderboards</h2>
 				<span class="text-lg text-slate-300">Bringing power back to the people</span>
 				<div class="mt-12 grid grid-cols-1 gap-4 text-left md:grid-cols-2">
-					<Advantage title="Design a Leaderboard" icon={AiOutlineUnorderedList}>
+					<Advantage title="Design a Leaderboard" icon={<AiOutlineUnorderedList />}>
 						Find out who comes out on top in <i>your</i> own custom leaderboard.
 						<br />
 						<A class="group relative mt-4 inline-block text-white hover:underline" href="/create">
@@ -20,10 +22,10 @@ export const InfoSection: Component = () => {
 							<AiOutlineArrowRight class="inline-block translate-x-1 transition-transform group-hover:translate-x-3" />
 						</A>
 					</Advantage>
-					<Advantage title="Use The Wisdom of the Crowd" icon={AiOutlineShareAlt}>
+					<Advantage title="Use The Wisdom of the Crowd" icon={<AiOutlineShareAlt />}>
 						Take advantage of the only reliable way to get the information you want.
 					</Advantage>
-					<Advantage title="Manage the Data" icon={AiOutlineControl}>
+					<Advantage title="Manage the Data" icon={<AiOutlineControl />}>
 						Recieve the best possible answer to your question.
 					</Advantage>
 					<div class="col-start-2 row-span-3 row-start-1 hidden place-items-center md:grid">
@@ -35,13 +37,11 @@ export const InfoSection: Component = () => {
 	);
 };
 
-const Advantage: Component<{ title: string; children: JSX.Element; icon: IconTypes }> = (props) => {
+const Advantage: Component<{ title: string; children: JSX.Element; icon: JSX.Element }> = (props) => {
 	return (
 		<div class="col-start-1">
 			<div class="flex items-center gap-4">
-				<span class="w-min rounded-md bg-red-700 p-2 text-2xl sm:text-3xl">
-					<Dynamic component={props.icon} />
-				</span>
+				<span class="w-min rounded-md bg-red-700 p-2 text-2xl sm:text-3xl">{props.icon}</span>
 				<h3 class="text-lg sm:text-2xl">{props.title}</h3>
 			</div>
 			<p class="mt-2 ml-14 text-sm text-slate-300 sm:ml-16 sm:mt-3 sm:text-base">{props.children}</p>
