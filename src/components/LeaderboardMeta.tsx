3d92dfc3be207bb513e511d91ed83b8a28681e7d
HatulaPro <shalev280804@gmail.com>
2023-02-10 16:37:37 +0200

    fix?

--- src/components/LeaderboardMeta.tsx
+++ src/components/LeaderboardMeta.tsx
@@ -4,7 +4,7 @@ import { Meta, Title } from 'solid-start';
 
 export const LeaderboardMeta: Component<{ leaderboard: Leaderboard }> = (props) => {
 	const title = () => `${props.leaderboard.name} - ${props.leaderboard.question} | Mostest`;
-	const description = () => `Vote and see the results of the Leaderboard of ${props.leaderboard.name}. ${props.leaderboard.question}. Only on Mostest.`;
+	const description = () => `Vote and see the results of the Leaderboard of ${props.leaderboard.name}. ${props.leaderboard.question}`;
 	return (
 		<>
 			<Title>{title()}</Title>
@@ -16,7 +16,8 @@ export const LeaderboardMeta: Component<{ leaderboard: Leaderboard }> = (props) 
 			<Meta property="og:url" content={`https://mostest.vercel.app/${props.leaderboard.slug}`} />
 			<Meta property="og:title" content={title()} />
 			<Meta property="og:description" content={description()} />
-			<Meta property="og:image" content={`https://mostest.vercel.app/api/${props.leaderboard.slug}`} />
+			<Meta property="og:image" itemProp="image" content={`https://mostest.vercel.app/api/${props.leaderboard.slug}`} />
+			<Meta property="og:site_name" content="Mostest" />
 
 			{/* Twitter */}
 			<Meta property="twitter:card" content="summary_large_image" />
--- src/routes/api/[slug].ts
+++ src/routes/api/[slug].ts
@@ -8,7 +8,7 @@ export async function GET(e: APIEvent) {
 	const slug = e.params['slug'];
 	if (!slug || typeof slug !== 'string') return redirect('/');
 
-	const [inter, interBold, leaderboard] = await Promise.all([interFont, interFontBold, prisma.leaderboard.findUnique({ where: { slug }, include: { options: { take: 5 } } })]);
+	const [inter, interBold, leaderboard] = await Promise.all([interFont, interFontBold, prisma.leaderboard.findUnique({ where: { slug }, include: { options: { take: 4 } } })]);
 	if (!leaderboard) return redirect('/');
 
 	const res = await satori(
@@ -16,7 +16,7 @@ export async function GET(e: APIEvent) {
 			type: 'div',
 			props: {
 				children: [
-					{ type: 'h1', props: { children: leaderboard.name, style: { fontSize: leaderboard.name.length > 10 ? (leaderboard.name.length > 20 ? '48px' : '54px') : '64px', fontWeight: '900', whiteSpace: 'pre-line' } } },
+					{ type: 'h1', props: { children: leaderboard.name, style: { fontSize: leaderboard.name.length > 10 ? (leaderboard.name.length > 20 ? '42px' : '48px') : '54px', fontWeight: '900', whiteSpace: 'pre-line', paddingRight: '40px' } } },
 					{ type: 'h3', props: { children: leaderboard.question, style: { fontSize: '32px', fontWeight: '400' } } },
 					{
 						type: 'div',
@@ -26,13 +26,13 @@ export async function GET(e: APIEvent) {
 								.map((option) => {
 									return { type: 'img', props: { src: option.image, style: { height: '96px', objectFit: 'contain' } } };
 								}),
-							style: { display: 'flex', gap: '10px', justifyContent: 'center', marginTop: 'auto' },
+							style: { display: 'flex', width: '100%', gap: '10px', justifyContent: 'center', marginTop: 'auto' },
 						},
 					},
-					{ type: 'img', props: { src: 'https://github.com/HatulaPro/mostest/blob/master/public/logo.svg?raw=true', style: { height: '64px', position: 'absolute', top: '12px', right: '12px' } } },
+					{ type: 'img', props: { src: 'https://github.com/HatulaPro/mostest/blob/master/public/logo.svg?raw=true', style: { height: '48px', position: 'absolute', top: '78px', right: '64px' } } },
 					{ type: 'span', props: { children: 'Powered by Mostest', style: { position: 'absolute', bottom: '12px', right: '12px', color: '#cbd5e1' } } },
 				],
-				style: { position: 'relative', color: 'white', backgroundImage: 'linear-gradient(to bottom, #1e293b, #111827)', fontFamily: 'Inter', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', padding: '24px' },
+				style: { position: 'relative', color: 'white', backgroundImage: 'linear-gradient(to bottom, #1e293b, #111827)', fontFamily: 'Inter', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', padding: '64px' },
 			},
 		},
 		{
