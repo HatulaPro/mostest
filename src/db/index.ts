b6436bbf7b6112bdc0c0f09f6259edadc1bbb3b9
HatulaPro <shalev280804@gmail.com>
2023-02-12 20:49:07 +0200

    >

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 import { PrismaClient } from '@prisma/client';
+import { serverEnv } from '~/env/server';
 
-export const prisma = new PrismaClient({ log: ['query'] });
+export const prisma = new PrismaClient({ log: serverEnv.NODE_ENV === 'development' ? ['query'] : [] });
