1deffdd43f6b7e3c90396288ac6252ddda7c43f2
HatulaPro <shalev280804@gmail.com>
2023-02-12 20:11:29 +0200

    edge?

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import vercel from 'solid-start-vercel';
 
 export default defineConfig({
-	plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
+	plugins: [solid({ ssr: true, adapter: vercel({ edge: true }) })],
 	ssr: { external: ['@prisma/client'] },
 });
