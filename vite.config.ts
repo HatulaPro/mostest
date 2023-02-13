cacfa502a7557cde50a4b87a39dfc8ce4ec2a14e
HatulaPro <shalev280804@gmail.com>
2023-02-13 14:14:51 +0200

    Revert "resvg"
    This reverts commit 5d2b698bab55249782c9e84367527bf03186690e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 import vercel from 'solid-start-vercel';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), solid({ ssr: true, adapter: vercel({ edge: false }) })],
+	plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
 	ssr: { external: ['@prisma/client'] },
 });
