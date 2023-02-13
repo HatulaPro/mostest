1149e30764a26e109bee2a44129c5e10317a02b3
HatulaPro <shalev280804@gmail.com>
2023-02-13 12:13:21 +0200

    You can now load more leaderboards

--- src/components/TopLeaderboards.tsx
+++ src/components/TopLeaderboards.tsx
@@ -1,13 +1,35 @@
 import { AiFillStar } from 'solid-icons/ai';
-import { type Component, For, Suspense } from 'solid-js';
-import { createServerData$ } from 'solid-start/server';
+import { type Component, For, Suspense, createEffect, Show } from 'solid-js';
+import { createStore, reconcile } from 'solid-js/store';
+import { createServerAction$, createServerData$ } from 'solid-start/server';
 import { prisma } from '~/db';
 import { LeaderboardView, LeaderboardViewLoading } from './LeaderboardView';
+import type { Leaderboard, Option } from '@prisma/client';
+import { Loading } from './Loading';
 
 export const TopLeaderboards: Component = () => {
 	const data = createServerData$(async () => {
 		return await prisma.leaderboard.findMany({ take: 3, include: { options: { take: 4, where: { image: { not: null } } } }, orderBy: { votes: { _count: 'desc' } } });
 	});
+
+	const [leaderboards, setLeaderboards] = createStore<
+		(Leaderboard & {
+			options: Option[];
+		})[]
+	>([]);
+
+	createEffect(() => {
+		const d = data();
+		if (!d) return;
+		setLeaderboards(reconcile(d, { key: 'id' }));
+	});
+
+	const [enrolling, enroll] = createServerAction$(
+		async (cursor: string) => {
+			return await prisma.leaderboard.findMany({ cursor: { id: cursor }, take: 3, skip: 1, include: { options: { take: 4, where: { image: { not: null } } } }, orderBy: { votes: { _count: 'desc' } } });
+		},
+		{ invalidate: ['nothing'] }
+	);
 
 	return (
 		<div class="w-full overflow-hidden bg-black/30">
@@ -17,10 +39,27 @@ export const TopLeaderboards: Component = () => {
 				</h2>
 				<div class="mx-auto grid max-w-5xl grid-cols-1 place-items-center gap-4 sm:grid-cols-3 sm:gap-8">
 					<Suspense fallback={<For each={[1, 2, 3]}>{() => <LeaderboardViewLoading />}</For>}>
-						<For each={data.latest}>{(leaderboard) => <LeaderboardView leaderboard={leaderboard} />}</For>
+						<Show when={data()}>
+							<For each={leaderboards.length ? leaderboards : data.latest}>{(leaderboard) => <LeaderboardView leaderboard={leaderboard} />}</For>
+						</Show>
 					</Suspense>
 				</div>
-				<button class="group relative mt-6 mb-2 inline-block text-white hover:underline">More Leaderboards</button>
+				{(enrolling.result ? enrolling.result.length > 2 : true) && (
+					<button
+						disabled={enrolling.pending}
+						onClick={() => {
+							if (leaderboards.length) {
+								enroll(leaderboards[leaderboards.length - 1].id).then((result) => {
+									setLeaderboards(reconcile([...leaderboards, ...result], { key: 'id' }));
+								});
+							}
+						}}
+						class="group relative mx-auto mt-6 mb-2 flex items-center gap-3 text-white hover:underline"
+					>
+						<Loading isLoading={enrolling.pending} />
+						More Leaderboards
+					</button>
+				)}
 			</div>
 		</div>
 	);
