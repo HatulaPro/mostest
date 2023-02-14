ecacd79d1b3cd657d5cb208ef9e4c5352dd90f06
HatulaPro <shalev280804@gmail.com>
2023-02-14 11:13:16 +0200

    Fixing export button

--- src/components/ExportCSV.tsx
+++ src/components/ExportCSV.tsx
@@ -25,7 +25,7 @@ export const ExportCSV: Component<{ leaderboardId: string }> = (props) => {
 
 	const close = () => {
 		batch(() => {
-			setOpen(true);
+			setOpen(false);
 			setCopied(false);
 			unsetTimeout();
 			setCopiedTimeout(null);
@@ -42,16 +42,21 @@ export const ExportCSV: Component<{ leaderboardId: string }> = (props) => {
 	return (
 		<>
 			<button
-				disabled={enrolling.pending}
+				// disabled={enrolling.pending}
 				class="text-xl text-gray-300 hover:text-white active:text-white"
 				onClick={() => {
 					if (enrolling.result) {
+						console.log(enrolling.result);
 						batch(() => {
 							setOpen(true);
 							setCopied(false);
 						});
 					} else {
 						enroll(props.leaderboardId);
+						batch(() => {
+							setOpen(true);
+							setCopied(false);
+						});
 					}
 				}}
 			>
@@ -59,7 +64,7 @@ export const ExportCSV: Component<{ leaderboardId: string }> = (props) => {
 			</button>
 			<Modal close={() => close} isOpen={isOpen()}>
 				<Loading isLoading={enrolling.pending} />
-				<p class="relative w-full overflow-hidden text-ellipsis border-2 border-gray-500 p-3 pr-10">
+				<p class="scrollbar relative w-full overflow-scroll border-2 border-gray-500 p-3 pr-10 max-h-80">
 					{enrolling.result}
 					<button
 						class="absolute right-1 top-1 rounded-md bg-slate-800 p-2 text-xs text-white"
